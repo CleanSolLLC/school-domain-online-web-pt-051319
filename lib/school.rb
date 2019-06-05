26317ea0c794d090f1c59c92c58ee17aaecd2e48
Mark May <mamark90@gmail.com>
2019-06-05 18:56:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,33 +1,28 @@
 require 'pry'
 
 class School
-  attr_accessor :school, :grade
-
-  def roster=(roster = {})
-    @roster = roster
-  end
-
-  def roster
-    @roster
-  end
+  attr_accessor :school, :grade, :roster
 
   def initialize(school_name)
     @school = school_name
+    def roster
+      roster = {}
+    end
   end
 
   def add_student(student_name, grade_num)
     @student_name = student_name
     @grade_num = grade_num
 
-    if @roster.empty?
-       @roster[grade_num] = []
-       @roster[grade_num] << student_name
+    if school.roster.empty?
+      school.roster[grade_num] = []
+      school.roster[grade_num] << student_name
 
-    elsif @roster.has_key? && roster[grade_num] == grade_num
-      @roster[grade_num] << student_name
+    elsif school.roster.has_key? && roster[grade_num] == grade_num
+      school.roster[grade_num] << student_name
 
     else
-      @roster[grade_num] = student_name
+      school.roster[grade_num] = student_name
     end
 
   end
