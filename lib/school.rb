9ca4f80096c7d3b2de3d6e21e13edac3c533cc96
Mark May <mamark90@gmail.com>
2019-06-05 18:01:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,11 +11,11 @@ class School
   def add_student(student_name, grade_num)
     @student_name = student_name
     @grade_num = grade_num
-    
+
     if roster.empty?
       roster[grade_num] = []
       roster[grade_num] << student_name
-      
+
     elsif roster.has_key? && roster[grade_num] == grade_num
       roster[grade_num] << student_name
 
