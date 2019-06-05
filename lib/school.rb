03a29699954c837d1d24900a921a09f4034701c7
Mark May <mamark90@gmail.com>
2019-06-05 18:17:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 class School
-  roster = {}
   #attr_accessor :school
   #attr_reader :grade
 
   def initialize(school_name)
     @school = school_name
+    roster = {}
   end
 
   def add_student(student_name, grade_num)
