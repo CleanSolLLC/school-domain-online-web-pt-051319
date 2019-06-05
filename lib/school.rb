fdcb7ab828557b96c1a32b2284c5c9d0c4be6b27
Mark May <mamark90@gmail.com>
2019-06-05 18:48:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,25 +5,30 @@ class School
 
   def initialize(school_name)
     @school = school_name
-    def roster
-      roster = {}
-      binding.pry
+    
+    def roster=(roster = {})
+      @roster roster
     end
+
+    def roster
+      @roster
+    end
+  
   end
 
   def add_student(student_name, grade_num)
     @student_name = student_name
     @grade_num = grade_num
 
-    if school.roster.empty?
-      school.roster[grade_num] = []
-      school.roster[grade_num] << student_name
+    if @roster.empty?
+       @roster[grade_num] = []
+       @roster[grade_num] << student_name
 
-    elsif school.roster.has_key? && roster[grade_num] == grade_num
-      school.roster[grade_num] << student_name
+    elsif @roster.has_key? && roster[grade_num] == grade_num
+      @roster[grade_num] << student_name
 
     else
-      school.roster[grade_num] = student_name
+      @roster[grade_num] = student_name
     end
 
   end
