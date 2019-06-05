f216f225b0d7692d2bdd885f5578170ef1ce4475
Mark May <mamark90@gmail.com>
2019-06-05 17:47:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+
+roster = {}
+
+  attrr_accessor :school_name,
+
+  def initialize(school_name)
+    @school_name = school_name
+  end
+
+  def add_student(student_name, grade_num)
+    @student_name = student_name
+    @grade_num = grade_num
+    if roster.empty?
+      roster[grade_num] = []
+      roster[grade_num] << student_name
+    elsif roster.has_key? && oster
+
+
+  end
+
+end
